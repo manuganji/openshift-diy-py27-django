de6b1a9764c329949f5c41f35dcdbcf3b12adb3a
manu <manu@manu-Inspiron-N5010.(none)>
2013-03-17 00:50:30 +0530

    added database params corresponding to a postgresql gear. 	configuring the media and static urls respectively in OPENSHIFT_DATA_DIR and OPENSHIFT_REPO_DIR 	modified:   example/example/settings/production.py

--- example/example/settings/production.py
+++ example/example/settings/production.py
@@ -9,7 +9,6 @@ from base import *
 # into your settings, but ImproperlyConfigured is an exception.
 from django.core.exceptions import ImproperlyConfigured
 
-
 def get_env_setting(setting):
 	""" Get the environment setting or return exception """
 	try:
@@ -17,8 +16,37 @@ def get_env_setting(setting):
 	except KeyError:
 		error_msg = "Set the %s env variable" % setting
 		raise ImproperlyConfigured(error_msg)
-		
-INSTALLED_APPS += ('gunicorn',)
+	
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = get_env_setting('OPENSHIFT_DATA_DIR')+'media/'
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = '/media/'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = get_env_setting('OPENSHIFT_REPO_DIR')+'static/'
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+# Put strings here, like "/home/html/static" or "C:/www/django/static".
+# Always use forward slashes, even on Windows.
+# Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_DIR, '..', 'static'),
+)
+
+
+	
+INSTALLED_APPS += ('uswgi',)
 
 ########## EMAIL CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
@@ -48,7 +76,18 @@ SERVER_EMAIL = EMAIL_HOST_USER
 
 
 ########## DATABASE CONFIGURATION
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'
+        'NAME': get_env_setting('OPENSHIFT_APP_NAME'),
+        'USER': get_env_setting('OPENSHIFT_POSTGRESQL_DB_USERNAME'),
+        'PASSWORD': get_env_setting('OPENSHIFT_POSTGRESQL_DB_PASSWORD'),
+        'HOST': get_env_setting('OPENSHIFT_POSTGRESQL_DB_HOST'),
+        'PORT': get_env_setting('OPENSHIFT_POSTGRESQL_DB_PORT'),
+        }
+}
+    
+
 ########## END DATABASE CONFIGURATION
 
 
